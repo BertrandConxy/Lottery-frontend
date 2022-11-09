6ed46b753c1715a436b06456a9356938272bdbc8
Bertrand <mutanganabertrand@gmail.com>
2022-11-09 18:17:39 +0200

    Create a copy of our contract

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import './App.css';
 import web3 from './web3';
+import { useEffect, useState } from 'react';
+import lottery from './lottery';
 
 function App() {
-  const getAccount = async () => {
-    const accounts = await web3.eth.getAccounts();
-    console.log(accounts);
-  };
+  const [manager, setManager] = useState('');
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState('');
+
+useEffect(() => {
+  async function fetchData() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    setManager(manager);
+    setPlayers(players);
+    setBalance(balance);
+  }
+  fetchData();
+}, []);
+
   return (
     <div className="App">
-      <button onClick={getAccount}>Get Accounts</button>
+      <h2>Lottery Contract</h2>
+      <div className='flex-container'>
+        <span>
+          This contract is managed by <strong>{manager}</strong>.
+        </span>
+        <span>
+          There are currently <strong>{players.length}</strong> people entered,
+        </span>
+        <span>
+          competing to win <strong>{web3.utils.fromWei(balance, 'ether')}</strong> ether!
+        </span>
+      </div>
     </div>
   );
 }
---
+++ src/lottery.js
@@ -0,0 +1,75 @@
+import Web3 from "web3";
+
+const address = "0xDD11Db60a297F04252194dB3e3112d754B464a0A";
+
+const abi = [
+	{
+		"inputs": [],
+		"name": "enter",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "pickWinner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "getPlayers",
+		"outputs": [
+			{
+				"internalType": "address[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "manager",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "players",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+const web3 = new Web3(window.ethereum);
+const lottery = new web3.eth.Contract(abi, address);
+
+export default lottery; 
