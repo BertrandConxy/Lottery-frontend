9a9d856c1c43595d3e740887c1d19310e74b5ea8
Bertrand <mutanganabertrand@gmail.com>
2022-11-09 17:19:07 +0200

    Add provider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
 
 function App() {
+  const getAccount = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getAccount}>Get Accounts</button>
     </div>
   );
 }
